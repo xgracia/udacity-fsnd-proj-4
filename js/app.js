f624fe78ac6f4b775178d8e6ab9b2a3d0a141264
Gracia, Xavier (xg146r) <xg146r@att.com>
2018-02-26 20:00:00 -0600

    style: Updates variable names

--- js/app.js
+++ js/app.js
@@ -1,34 +1,34 @@
 var app = null;
 var map = null;
-var map_markers = [];
+var mapMarkers = [];
 var places = [{
     name: 'Taco Ocho',
-    yelp_id: 'taco-ocho-richardson',
-    yelp_data: null,
+    yelpId: 'taco-ocho-richardson',
+    yelpData: null,
     lat: 32.974799,
     lng: -96.709017
 },{
     name: 'Olive Burger',
-    yelp_id: 'olive-burger-richardson-richardson',
-    yelp_data: null,
+    yelpId: 'olive-burger-richardson-richardson',
+    yelpData: null,
     lat: 32.960417,
     lng: -96.734356
 },{
     name: 'Ye Ole Butcher Shop',
-    yelp_id: 'ye-ole-butcher-shop-plano',
-    yelp_data: null,
+    yelpId: 'ye-ole-butcher-shop-plano',
+    yelpData: null,
     lat: 33.020294,
     lng: -96.704346
 },{
     name: 'El Pueblito',
-    yelp_id: 'el-pueblito-mexican-cocina-plano',
-    yelp_data: null,
+    yelpId: 'el-pueblito-mexican-cocina-plano',
+    yelpData: null,
     lat: 33.056949,
     lng: -96.710714
 },{
     name: "Del's Charcoal Burgers",
-    yelp_id: 'dels-charcoal-burgers-richardson',
-    yelp_data: null,
+    yelpId: 'dels-charcoal-burgers-richardson',
+    yelpData: null,
     lat: 32.948137,
     lng: -96.731356
 }];
@@ -36,14 +36,14 @@ var places = [{
 function AppViewModel(){
     var self = this;
 
-    this.places_list = ko.observableArray(places);
+    this.placesList = ko.observableArray(places);
 
-    this.selected_place = ko.observable(null);
+    this.selectedPlace = ko.observable(null);
 
     this.search = ko.observable('');
 
     // returns filtered list of places based on search observable
-    this.filtered_places = function(){
+    this.filteredPlaces = function(){
         var query = new RegExp(self.search(), 'i');
 
         return places.filter(function(place){
@@ -51,21 +51,21 @@ function AppViewModel(){
         });
     };
 
-    // update places_list based on filtered list
+    // update placesList based on filtered list
     // also clears selected place if not in filtered list
     this.search.subscribe(function(){
-        var filtered_places = self.filtered_places();
-        var selected_place = self.selected_place();
+        var filteredPlaces = self.filteredPlaces();
+        var selectedPlace = self.selectedPlace();
 
-        self.places_list(filtered_places);
+        self.placesList(filteredPlaces);
 
-        if(filtered_places.indexOf(selected_place) < 0){
-            self.selected_place(null);
+        if(filteredPlaces.indexOf(selectedPlace) < 0){
+            self.selectedPlace(null);
         }
     });
 
     // animate Google Map Marker upon selection
-    this.selected_place.subscribe(renderMarkers);
+    this.selectedPlace.subscribe(renderMarkers);
 
     // add Google Map Markers based on filtered list of places
     this.search.subscribe(renderMarkers);
@@ -88,43 +88,43 @@ function renderMarkers(){
     }
 
     // clear all existing map markers
-    map_markers.forEach(function(marker){
+    mapMarkers.forEach(function(marker){
         marker.setMap(null);
     });
-    map_markers = [];
+    mapMarkers = [];
 
-    // append markers for each filtered_place into map_markers
-    var filtered_places = app.filtered_places();
-    filtered_places.forEach(function(place){
+    // append markers for each filtered_place into mapMarkers
+    var filteredPlaces = app.filteredPlaces();
+    filteredPlaces.forEach(function(place){
         var animation = null;
-        var info_window = null;
+        var infoWindow = null;
 
         // add animation if place is selected
         // also builds an info window to be displayed
-        if(place == app.selected_place()){
+        if(place == app.selectedPlace()){
             animation = google.maps.Animation.BOUNCE;
 
             var html = '<h5>' + place.name + '</h5>';
             // if yelp data hasn't loaded yet, link the yelp page
-            if(place.yelp_data === null){
+            if(place.yelpData === null){
                 html += '<p>Still retrieving Yelp Data. Please try again soon or visit: ' +
-                    '<a href="https://www.yelp.com/biz/' + place.yelp_id + '">Its Yelp Page</a>' +
+                    '<a href="https://www.yelp.com/biz/' + place.yelpId + '">Its Yelp Page</a>' +
                     '</p>';
             }
             // if something went wrong w/ the yelp request, link to the yelp page
-            else if(place.yelp_data.error){
+            else if(place.yelpData.error){
                 html += '<p>Error retrieving Yelp Data. Please visit: ' +
-                    '<a href="https://www.yelp.com/biz/' + place.yelp_id + '">Its Yelp Page</a>' +
+                    '<a href="https://www.yelp.com/biz/' + place.yelpId + '">Its Yelp Page</a>' +
                     '</p>';
             }
             // if all went right, show rating and review counts
             else{
-                html += '<p>Rating: ' + place.yelp_data.rating + '</p>' +
-                    '<p>Reviews: ' + place.yelp_data.review_count + '</p>' +
-                    '<p><a href="https://www.yelp.com/biz/' + place.yelp_id + '">Its Yelp Page</a></p>';
+                html += '<p>Rating: ' + place.yelpData.rating + '</p>' +
+                    '<p>Reviews: ' + place.yelpData.review_count + '</p>' +
+                    '<p><a href="https://www.yelp.com/biz/' + place.yelpId + '">Its Yelp Page</a></p>';
             }
 
-            info_window = new google.maps.InfoWindow({
+            infoWindow = new google.maps.InfoWindow({
                 content: html
             });
         }
@@ -136,21 +136,21 @@ function renderMarkers(){
             animation: animation
         });
 
-        // Update selected_place when a marker is clicked
+        // Update selectedPlace when a marker is clicked
         marker.addListener('click', function(){
-            app.selected_place(place);
+            app.selectedPlace(place);
         });
 
-        map_markers.push(marker);
+        mapMarkers.push(marker);
 
         // display the info window if it's defined
-        if(info_window !== null){
-            info_window.open(map, marker);
+        if(infoWindow !== null){
+            infoWindow.open(map, marker);
 
             // deselect the place when closing the info window
-            info_window.addListener('closeclick', function(){
-                info_window.marker = null;
-                app.selected_place(null);
+            infoWindow.addListener('closeclick', function(){
+                infoWindow.marker = null;
+                app.selectedPlace(null);
             });
         }
     });
@@ -159,10 +159,10 @@ function renderMarkers(){
 // function gets Yelp data for all places
 function getYelpData(){
     places.forEach(function(place){
-        $.getJSON('https://udacity-fsnd-proj4.my.to/yelp_data/' + place.yelp_id, function(data){
-            place.yelp_data = data;
+        $.getJSON('https://udacity-fsnd-proj4.my.to/yelpData/' + place.yelpId, function(data){
+            place.yelpData = data;
         }).fail(function(){
-            place.yelp_data = {error: true};
+            place.yelpData = {error: true};
         });
     });
 }
